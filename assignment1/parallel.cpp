7e20cda5266584cc25fa0c349256168d5e5d0323
Sergio Gomez <segomez1304@hotmail.com>
2016-10-24 19:22:34 -0700

    started working on parallel

---
+++ assignment1/parallel.cpp
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+	string urls;
+
+	pid_t pid;
+
+	return 0;
+}
